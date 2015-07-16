42eab0c9eb8112ed23306e5d2f1c8b5884677b95
Tomas Sieger <tsieger@server.fake>
2015-07-16 09:04:47 +0200

    disable asking for replot

--- demo/idendroWithScatter.R
+++ demo/idendroWithScatter.R
@@ -15,8 +15,10 @@ colorizeCallback <- function(clr) {
     # color the scatter plot according to the current clusters
     clusterColors <- c('black','red', 'green', 'blue', 'yellow', 'magenta',
         'cyan', 'darkred', 'darkgreen', 'purple', 'darkcyan')
+    opar <- par(ask = FALSE)
     plot(iris$Sepal.Length, iris$Sepal.Width,
         col = clusterColors[clr + 1], pch = 19)
+    par(opar)
 }
 
 # visualize clusters and heat map
