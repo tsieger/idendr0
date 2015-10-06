d7da53bf5e8645033146d36e4d1393c8f3da0b77
Tomas Sieger <tsieger@server.fake>
2015-10-06 21:16:11 +0200

    removed unused

--- R/idendro-internal.R
+++ R/idendro-internal.R
@@ -1,32 +1,6 @@
 ### Internal functions.
 
-.ls.objects <-
-function (pos = 1, pattern, order.by = "Size", decreasing=TRUE, head =     TRUE, n = 10) {
-  # based on postings by Petr Pikal and David Hinds to the r-help list in 2004
-  # modified by: Dirk Eddelbuettel (http://stackoverflow.com/questions/1358003/tricks-to-manage-the-available-memory-in-an-r-session)
-  # I then gave it a few tweaks (show size as megabytes and use defaults that I like)
-  # a data frame of the objects and their associated storage needs.
-  napply <- function(names, fn) sapply(names, function(x)
-          fn(get(x, pos = pos)))
-  names <- ls(pos = pos, pattern = pattern)
-  obj.class <- napply(names, function(x) as.character(class(x))[1])
-  obj.mode <- napply(names, mode)
-  obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
-  obj.size <- napply(names, object.size) / 10^6 # megabytes
-  obj.dim <- t(napply(names, function(x)
-            as.numeric(dim(x))[1:2]))
-  vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
-  obj.dim[vec, 1] <- napply(names, length)[vec]
-  out <- data.frame(obj.type, obj.size, obj.dim)
-  names(out) <- c("Type", "Size", "Rows", "Columns")
-  out <- out[order(out[[order.by]], decreasing=decreasing), ]
-  if (head)
-    out <- head(out, n)
-  out
-}
-
 .dbg.internal<-0
-
 
 mySeq <- function
 ### Sequence generation resembling matlab ':' operator.
@@ -42,15 +16,6 @@ by=1##<< step (defaults to 1)
     if ((from<to) == (by>0)) return(seq(from,to,by))
     else if (from==to) return(from)
     else return(vector(class(from),0))
-}
-
-printWithName<-function
-### Print the name and value of a variable.
-##keyword<<internal
-(x##<< variable to print
-) {
-    cat(paste(deparse(substitute(x)),'\n',sep=''))
-    print(x)
 }
 
 printVar<-function
@@ -125,39 +90,4 @@ required=TRUE##<< Require this variable and err if not found? If FALSE,
         #return(attr(rv$value,'.sharedEnv')[deparse(substitute(nm))])
         return(NULL)
     }
-}
-
-last<-function
-###Extract the first element of the argument.
-##keyword<<internal
-(x) tail(x,1)
-
-first<-function
-###Extract the last element of the argument.
-##keyword<<internal
-(x) head(x,1)
-
-.sharedVarNames<-function() {
-    list(
-    'dbg',
-    'dbg.args',
-    'dbg.mouse',
-    'dbg.gui',
-    'dbg.tx',
-    'dbg.dendro',
-    'dbg.dendro.zoom',
-    'dbg.dendro.axis',
-    'dbg.dendro.limits',
-    'dbg.dendro.select',
-    'dbg.dendro.cut',
-    'dbg.dendro.info',
-    'dbg.axis',
-    'dbg.heatmap',
-    'dbg.heatmap.text',
-    'dbg.heatmap.limits',
-    'dbg.brushedmap',
-    'dendroZoom',
-    'dendroZoomMouseSelection',
-    'params',
-    'df')
 }
